e90323e2bc3e047c992fd06646baf23d18728d3f
Ahmet Buğra ÖNEN <ahmbgronn@gmail.com>
2021-04-26 12:24:02 +0300

    Add project files.

---
+++ ClassMethodDemo/CustomerManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMethodDemo
+{
+    class CustomerManager
+    {
+        public void Attach(Customer customer)
+        {
+            Console.WriteLine(customer.Name + " " + customer.Surname + ":" + "Successful! Customer Added.");
+            Console.WriteLine(customer.TC + ":");
+        }
+        public void Delete(Customer customer)
+        {
+            Console.WriteLine(customer.Name + customer.Surname + ":" + "Succesful! Customer Deleted.");
+
+        }
+
+    }
+}
---
+++ ClassMethodDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ClassMethodDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer0 = new Customer();
+            customer0.Name = "Behzat";
+            customer0.Surname = "Ç.";
+            customer0.TC = 11111111111;
+            Customer customer1 = new Customer();
+            customer1.Name = "Hayalet";
+            customer1.Surname = "H";
+            customer1.TC = 11111111111;
+            Customer customer2 = new Customer();
+            customer2.Name = "Akbaba";
+            customer2.Surname = "A";
+            customer2.TC = 11111111111;
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Attach(customer0);
+            customerManager.Attach(customer1);
+            customerManager.Attach(customer2);
+        }
+    }
+}
